ad484d25848dc435bedac02d5b56bab9bb84b62d
tiesen243 <ttien56906@gmail.com>
2024-07-22 05:14:16 +0700

    feat(page.tsx): update UI and add logout functionality
    This commit updates the UI for unauthorized access and user welcome
    messages. It also replaces the old button component with a new one.
    Additionally, it introduces a logout functionality that allows users
    to sign out from the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,35 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 
-import { buttonVariants } from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
 import { auth } from '@/server/auth'
+import { api } from '@/lib/trpc/server'
 
 const Page: NextPage = async () => {
   const { user } = await auth()
   if (!user)
     return (
       <div className="flex min-h-svh flex-col items-center justify-center gap-4">
-        <p>Unauthorized access. Please sign in.</p>
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign in
-        </Link>
+        <p className="text-lg font-medium">Unauthorized access. Please sign in.</p>
+        <Button asChild>
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
       </div>
     )
 
-  return <div className="grid min-h-dvh place-items-center">{user.name}</div>
+  const action = async () => {
+    'use server'
+    await api.auth.signOut()
+  }
+
+  return (
+    <div className="flex min-h-dvh flex-col items-center justify-center gap-4">
+      <p className="text-lg font-medium">Welcome, {user.name}!</p>
+      <form action={action}>
+        <Button>Log out</Button>
+      </form>
+    </div>
+  )
 }
 
 export default Page
