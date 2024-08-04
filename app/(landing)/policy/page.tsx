b65b20c1e28bea2faa5363c2d432aabf8a511e50
tiesen243 <ttien56906@gmail.com>
2024-08-04 15:28:49 +0700

    refactor: simplify container classes in policy page
    - Merged container classes for header and main sections
    - Removed redundant div wrappers
    - Improved readability and maintainability of the code

--- app/(landing)/policy/page.tsx
+++ app/(landing)/policy/page.tsx
@@ -15,29 +15,23 @@ export const metadata = {
 
 const Page: NextPage = () => (
   <>
-    <header className="bg-muted px-4 py-6 sm:px-6">
-      <div className="container">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
-          {policy.title}
-        </h1>
-      </div>
+    <header className="container bg-muted py-6">
+      <h1 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
+        {policy.title}
+      </h1>
     </header>
 
-    <main className="flex-1 px-4 py-12 sm:px-6">
-      <div className="container space-y-12">
-        {policy.content.map((policy) => (
-          <section key={policy.title}>
-            <h2 className="mb-4 text-2xl font-bold tracking-tight text-foreground">
-              {policy.title}
-            </h2>
-            <div className="space-y-2 text-muted-foreground">
-              {policy.content.map((content) => (
-                <p key={content}>{content}</p>
-              ))}
-            </div>
-          </section>
-        ))}
-      </div>
+    <main className="container flex-1 space-y-12 py-12">
+      {policy.content.map((policy) => (
+        <section key={policy.title}>
+          <h2 className="mb-4 text-2xl font-bold tracking-tight text-foreground">{policy.title}</h2>
+          <div className="space-y-2 text-muted-foreground">
+            {policy.content.map((content) => (
+              <p key={content}>{content}</p>
+            ))}
+          </div>
+        </section>
+      ))}
     </main>
   </>
 )
