531e7ad0b962941855738e961ce7831cecfe4ef0
tiesen243 <ttien56906@gmail.com>
2024-09-07 09:16:53 +0700

    feat(auth): add user authentication check in layout
    - Import `redirect` from 'next/navigation' and `auth` from '@/server/auth'
    - Modify `AuthLayout` to be an async function component
    - Add user authentication check using `auth` function
    - Redirect authenticated users to the home page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
-const AuthLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <main className="grid min-h-dvh place-items-center">
-    <div className="container max-w-screen-md rounded-lg p-6 shadow-lg md:border">{children}</div>
-  </main>
-)
+import { redirect } from 'next/navigation'
+
+import { auth } from '@/server/auth'
+
+const AuthLayout: React.FC<React.PropsWithChildren> = async ({ children }) => {
+  const { user } = await auth()
+  if (user) redirect('/')
+
+  return (
+    <main className="grid min-h-dvh place-items-center">
+      <div className="container max-w-screen-md rounded-lg p-6 shadow-lg md:border">{children}</div>
+    </main>
+  )
+}
 
 export default AuthLayout
