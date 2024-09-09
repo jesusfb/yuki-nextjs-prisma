7fd9a6fbce7fd1cde7886ce91e89356f34cae413
tiesen243 <ttien56906@gmail.com>
2024-09-09 15:01:58 +0700

    feat: update layout and page structure
    - Removed `LatestPost` component from `post.tsx`
    - Updated global styles in `globals.css`
    - Modified `layout.tsx` to include `ThemeProvider`
    - Simplified `page.tsx` to a basic structure
    - Added new dependencies in `package.json`

---
+++ app/sitemap.ts
@@ -0,0 +1,37 @@
+import type { MetadataRoute } from 'next'
+
+import { db } from '@/server/db'
+import { getBaseUrl } from '@/lib/utils'
+
+interface Route {
+  url: string
+  lastModified: string
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Fetch static routes
+  const routesMap: Route[] = ['', 'about', 'contact', 'privacy-policy', 'terms-of-service'].map(
+    (route) => ({
+      url: `${getBaseUrl()}/${route}`,
+      lastModified: new Date().toISOString(),
+    }),
+  )
+
+  // const userRoutes: Route[] = await db.user
+  //   .findMany({ select: { id: true, name: true, updatedAt: true } })
+  //   .then((users) =>
+  //     users.map((user) => ({
+  //       url: `${getBaseUrl()}/u/${createSlug({ str: user.name, suffix: user.id })}`,
+  //       lastModified: user.updatedAt.toISOString(),
+  //     })),
+  //   )
+
+  // Fetch dynamic routes
+  let fetchedRoutes: Route[] = []
+  try {
+    fetchedRoutes = (await Promise.all([])).flat()
+  } catch (error) {
+    if (error instanceof Error) throw new Error(`Error fetching dynamic routes: ${error.message}`)
+  }
+  return [...routesMap, ...fetchedRoutes]
+}
---
+++ lib/seo.ts
@@ -0,0 +1,35 @@
+import { type Metadata } from 'next'
+import { type OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
+import { getBaseUrl } from './utils'
+
+interface Params {
+  title?: string
+  description?: string
+  images?: OpenGraph['images']
+  url?: string
+}
+
+export const seo = (params: Params): Metadata => {
+  const title = params.title ? `${params.title} | Yuki` : 'Yuki'
+  const description =
+    params.description ??
+    'Yuki is a full-stack e-commerce platform built with Next.js, Prisma, and tRPC. It is a modern, fast, and secure platform that allows you to create your own e-commerce store with ease. Yuki is built with the latest technologies and best practices to ensure that your store is fast, secure, and scalable.'
+  const images = params.images ?? ['/api/og']
+  const url = params.url ? `${getBaseUrl()}/${params.url}` : getBaseUrl()
+
+  return {
+    metadataBase: new URL(getBaseUrl()),
+    title,
+    description,
+    creator: 'Tiesen',
+    category: 'E-commerce',
+    applicationName: 'Yuki',
+    alternates: { canonical: url },
+    facebook: { appId: '523462826928110' },
+    authors: { name: 'Tiesen', url: 'https://tiesen.id.vn' },
+    keywords: ['Yuki', 'E-commerce', 'Next.js', 'Prisma', 'tRPC'],
+    openGraph: { url, images, type: 'website', siteName: 'Yuki' },
+    twitter: { card: 'summary_large_image', creatorId: '@tiesen243' },
+    icons: { icon: '/favicon.ico', shortcut: '/favicon-16x16.png', apple: '/apple-touch-icon.png' },
+  }
+}
