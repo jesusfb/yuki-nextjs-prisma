aa3186d69fd2f07d83d9a06f1b7904a25e33fc29
tiesen243 <ttien56906@gmail.com>
2024-09-14 00:18:57 +0700

    feat(eslint): add next/typescript to eslint config
    Added 'next/typescript' to the ESLint configuration to enhance TypeScript
    support for Next.js projects. This ensures better linting and code quality
    for TypeScript files in the project.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ const config = {
   parserOptions: { project: true },
   plugins: ['@typescript-eslint'],
   extends: [
+    'next/typescript',
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
