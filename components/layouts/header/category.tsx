2872a78891274f0d5b5ec1e29e40e47c4d339df1
tiesen243 <ttien56906@gmail.com>
2024-09-16 08:02:44 +0700

    feat(header): add slugify to category links
    - Import `slugify` from `@/lib/utils`
    - Update category links to use slugified names with IDs
    - Ensure links point to `/search?category=slugified-name-id`

--- components/layouts/header/category.tsx
+++ components/layouts/header/category.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 import { api } from '@/lib/trpc/server'
-import { cn } from '@/lib/utils'
+import { cn, slugify } from '@/lib/utils'
 
 export const Category: React.FC<Props> = async ({ className, itemClassName }) => {
   const categories = await api.category.getAll({ limit: 3 })
@@ -10,7 +10,9 @@ export const Category: React.FC<Props> = async ({ className, itemClassName }) =>
     <ul className={cn('flex gap-2', className)}>
       {categories.slice(0, 3).map((category) => (
         <li key={category.id} className={itemClassName}>
-          <Link href="#">{category.name}</Link>
+          <Link href={`/search?category=${slugify(category.name, category.id)}`}>
+            {category.name}
+          </Link>
         </li>
       ))}
     </ul>
